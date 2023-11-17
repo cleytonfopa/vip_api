07c508afa50d56d5237bd02ceffbb016c4957ee4
Cleyton Fariasb <cleytonfarias@outlook.com>
2023-11-17 16:23:23 -0300

    new api version: new model

--- api.py
+++ api.py
@@ -1,7 +1,7 @@
 import sys
 from flask import Flask, request
 from joblib import load
-from utils import f, clean_DF, calculate_recency, calculate_scores
+from utils import calculate_recency
 import pandas as pd
 import numpy as np
 import datetime
@@ -14,51 +14,52 @@ def predict():
     json_ = request.get_json()
     # convert to DF:
     df = pd.DataFrame(json_)
-    # cleaning:
-    df = clean_DF(df)    
-    # Calculanto agregados por dia:
-    bet_by_day = (
+    df["data"] = pd.to_datetime(df["data"])
+    df["Deposit_Date"] = pd.to_datetime(df["Deposit_Date"])
+    # Calculate Recency:
+    recency_df = (
       df
-      .groupby(["Username", "registration_dt", "ftd_value","age", "date"])
-      .agg(
-        n_bets=("turnover", lambda x: np.sum(x>0)), 
-        turnover=("turnover", np.sum),  
-        ggr=("ggr", np.sum)
-        )
-      .reset_index()
-    )
-    bet_by_day
-    # selecionando variáveis:
-    bet_by_day = bet_by_day[["registration_dt", "date", "Username", "ftd_value", "age", "n_bets", "turnover", "ggr"]]
-    bet_by_day = bet_by_day.sort_values(by=["registration_dt", "Username"])
-    # numero máximo de dias de atividade na plataforma:
-    max_days_sample=(bet_by_day["date"].max() - bet_by_day["registration_dt"].min()).days + 1
-    # Criando as datas correntes para todos os usuários
-    bet_by_day = (
-      bet_by_day
       .groupby("Username")
-      .apply(f,min_days=max_days_sample)
-      .reset_index(drop=True)
+      .apply(
+        calculate_recency, 
+        date_max=datetime.datetime.today()
+      )
     )
-    # Filtrando para a data máxima de apostas da base:
-    dt_max_bet = df["date"].max()
-    bet_by_day = bet_by_day.query("date <= @dt_max_bet")
-    # calculating covariates:
-    rfm_df = calculate_scores(bet_by_day, bet_by_day["date"].max())
-    # adding user info:
+    recency_df = recency_df.reset_index()
+    recency_df.columns = ["Username", "recency_value"]
+    ## Calculate Frequency:
+    frequency_df = df.groupby("Username")["n_bets"].sum()
+    frequency_df = frequency_df.reset_index()
+    frequency_df.columns = ["Username", "frequency_value"]
+    ## Calculate Monetary value:
+    monetary_df = df.groupby("Username")["ggr"].sum()
+    monetary_df = monetary_df.reset_index()
+    monetary_df.columns = ["Username", "monetary_value"]
+    # Calculate turnover value:
+    turnover_df = df.groupby("Username")["turnover"].sum()
+    turnover_df = turnover_df.reset_index()
+    turnover_df.columns = ["Username", "turnover_value"]
+    # merging:
+    rfm_df = recency_df.merge(frequency_df, on="Username")
+    rfm_df = rfm_df.merge(monetary_df, on="Username")
+    rfm_df = rfm_df.merge(turnover_df, on="Username")
+    # calculando ticket medio
+    rfm_df["ticket_medio"] = rfm_df["turnover_value"] / rfm_df["frequency_value"]
+    # fill na with 0:
+    rfm_df["ticket_medio"] = rfm_df["ticket_medio"].fillna(0)
+    # adding info about the player:
     rfm_df = rfm_df.merge(
-        bet_by_day[["Username", "age", "ftd_value"]].drop_duplicates(),
+        df[["Username", "age", "ftd_value"]].drop_duplicates(),
         on="Username"
-    ) 
+    )    
     ## predicting:
     vars_ = [
-        'recency_value', 'frequency_value', 'revenue_value', 'turnover_value',
-       'ticket_medio', 'overall_score', 'ftd_value', 'age'
+      'recency_value', 'frequency_value', 'monetary_value', 'turnover_value',
+      'ticket_medio', 'ftd_value', 'age'
     ]
     # predicting
     rfm_df["vip_proba"] = clf.predict_proba(rfm_df[vars_])[:, 1]
     rfm_df["vip_proba"] = np.where(rfm_df["vip_proba"] == 1, .999, rfm_df["vip_proba"])
-    rfm_df["vip_proba"] = rfm_df["vip_proba"].round(3)
     # returning:
     return rfm_df[["Username", "vip_proba"]].to_json(orient="records")
 
@@ -70,7 +71,7 @@ if __name__ == '__main__':
     except:
         port = 1234
     # loading models:
-    clf = load("clf_vip_model.joblib")
+    clf = load("vip_model.joblib")
     # running debug mode:
     app.run(port=port, debug=True)
 
